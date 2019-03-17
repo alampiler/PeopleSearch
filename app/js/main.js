060c2caae64a3c7d9034b1d3246dddb631f16c5b
alampiler <alampil33149@gmail.com>
2019-03-17 18:38:26 +0200

    Add responsive styles

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,46 @@ $(document).ready(function () {
     slidesToScroll: 6,
     autoplay: true,
     autoplaySpeed: 2000,
-    centerMode: true
+    centerMode: true,
+      responsive: [
+          {
+              breakpoint: 1028,
+              settings: {
+                  centerPadding: '20px',
+                  slidesToShow: 6,
+                  slidesToScroll: 6
+              }
+          },
+
+          {
+              breakpoint: 830,
+              settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4,
+                  centerPadding: '10px'
+              }
+          },
+
+          {
+              breakpoint: 670,
+              settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4,
+                  centerPadding: '1px'
+              }
+          },
+
+          {
+              breakpoint: 520,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  centerPadding: '2px'
+              }
+          }
+
+
+      ]
   });
 });
 
